2990e974dec48fc0f908de804317aab20249e4c8
Mahidiwan <mahidiwan2812@gmail.com>
2026-01-08 15:37:35 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,27 @@
+#subtract two numbers
+
+def add(a,b):
+    c=a+b
+    print(c)
+
+def subtract(a,b):
+    c=a-b
+    print(c)
+
+def multiplication(a,b):
+    c=a*b
+    print(c)
+
+def divide(a,b):
+    c=a/b
+    print(c)
+
+#main
+a=int(input("enter number"))
+b=int(input("enter another number"))
+add(a,b)
+subtract(a,b)
+multiplication(a,b)
+divide(a,b)
+      
+    
