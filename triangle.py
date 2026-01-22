cf67584f36f98d0e9e1256aaf47a14679e48b364
Mahidiwan <mahidiwan2812@gmail.com>
2026-01-22 15:07:32 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,8 @@
+#check if triangle or not
+a=int(input("enter first angle:"))
+b=int(input("enter second angle:"))
+c=int(input("enter third angle:"))
+if a>0 and b>0 and c>0 and (a+b+c==180):
+    print("valid triangle")
+else:
+    print("not a valid triangle")
